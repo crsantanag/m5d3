5dc1685a0a7f4aeb16c4fec31f9da9ec8c99078c
Carlos Santana <carlos.santanag@live.cl>
2024-09-28 01:44:01 -0300

    v6.0 - annotate

--- app/models/documentary.rb
+++ app/models/documentary.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: documentaries
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  synopsis   :string
+#  director   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Documentary < ApplicationRecord
   validates :name,     presence: true, length: {in:2..50}
   validates :director, presence: true, length: {in: 2..50}
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: series
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  synopsis   :string
+#  director   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Serie < ApplicationRecord
   validates :name,     presence: true, length: {in:2..50}
   validates :director, presence: true, length: {in: 2..50}
